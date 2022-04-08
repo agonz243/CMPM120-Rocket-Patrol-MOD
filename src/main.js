f0128cb2127c17b2d48e5ece820f80256b673f83
Aaron Gonzales <agonz243@ucsc.edu>
2022-04-07 18:53:35 -0700

    Added Menu and Play scenes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
+
+// Define global variables for UI size
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
