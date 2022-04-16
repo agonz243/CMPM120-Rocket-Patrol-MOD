ecfe279d8294ffd4b5d7915da781fe1798eb7478
Aaron Gonzales <agonz243@ucsc.edu>
2022-04-16 12:46:53 -0700

    Added submission comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+/*
+*   Name: Aaron Gonzales
+*   Project Title: Rocket Patrol Mod
+*   Date: Apr 16th, 2022
+*   Time to Complete: 6 hours
+*
+*
+*   Point Breakdown:
+*   Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+*   Implement mouse control for player movement and mouse click to fire (20)
+*   Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+*   Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+*   Implement parallax scrolling (10)
+*   Display the time remaining (in seconds) on the screen (10)
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
